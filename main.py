e6fc64ca1a70e644bdb247f6be3fb34685eed47c
sai <sai@mail.ru>
2024-02-20 17:05:30 +0700

    Отправка первого сообщения

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import pprint
 with open('token.txt') as f:
     token = f.read()
 
-endPoint = f'https://api.telegram.org/bot{token}/getMe'
-res = requests.get(endPoint).json()
-pprint.pprint(res)
+#получить информацию по всем событиям (апдейтам)
+endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+response = requests.get(endPoint).json()['result']
+pprint.pprint(response)
 
+#сформировать приветствие и узнать chat_id
+mes = f'Привет, {response[0]['message']['from']['first_name']}!'
+chatID = response[0]['message']['chat']['id']
+
+#отправить сообщение в чат
+endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+params = {'chat_id': chatID, 'text': mes}
+response = requests.get(endPoint, params=params)
