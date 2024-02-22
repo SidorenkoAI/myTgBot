30ee70cba20250403e471dac455c8c1277cc4d3b
sai <sai@mail.ru>
2024-02-22 20:20:01 +0700

    Отправка всем привет

--- main.py
+++ main.py
@@ -8,11 +8,20 @@ endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
 response = requests.get(endPoint).json()['result']
 pprint.pprint(response)
 
-#сформировать приветствие и узнать chat_id
-mes = f'Привет, {response[0]['message']['from']['first_name']}!'
-chatID = response[0]['message']['chat']['id']
+usersInfo = dict()
+for i in response:
+    chatID = i['message']['chat']['id']
+    userName = i['message']['chat']['first_name']
+    if 'text' in i['message']:
+        userText = i['message']['text']
+    usersInfo[chatID] = [userName, userText]
 
+
+print(usersInfo)
 #отправить сообщение в чат
 endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-params = {'chat_id': chatID, 'text': mes}
-response = requests.get(endPoint, params=params)
+for user in usersInfo:
+    mes = f'Привет, {usersInfo[user][0]}!'
+    params = {'chat_id': user, 'text': mes}
+    res = requests.get(endPoint, params=params)
+
