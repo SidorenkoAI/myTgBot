0dfd0607d4e50192655225ae17caa078a56c6f2b
sai <sai@mail.ru>
2024-02-22 21:20:06 +0700

    АстроФото без проверки на дату

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ with open('token.txt') as f:
 # a = [1,2,3,4,5,6,7,8]
 # print(a[-1:])
 
+def givePhoto(date: str) -> (str, str):
+    endPoint = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': 'DEMO_KEY', 'date': date}
+    res = requests.get(endPoint, params=params).json()
+    explanation = res['explanation']
+    urlPhoto = res['url']
+    return (urlPhoto, explanation)
+
+
 import time
 offset = -2
 while True:
@@ -23,13 +32,30 @@ while True:
         offset = response['result'][0]['update_id']
         userText = response['result'][0]['message']['text']
         chatID = response['result'][0]['message']['chat']['id']
-        pprint.pprint(response)
+        photoURL, photoExp = givePhoto(userText)
+        endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+        params = {'chat_id': chatID, 'photo': photoURL}
+        res = requests.get(endPoint, params=params)
         endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-        params = {'chat_id': chatID, 'text': userText}
+        params = {'chat_id': chatID, 'text': photoExp}
         res = requests.get(endPoint, params=params)
     time.sleep(1)
 
 
+
+
+# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+# res = requests.get(endPoint)
+# pprint.pprint(res.json())
+# endPoint = f'https://api.telegram.org/bot{token}/sendPhoto'
+# params = {'chat_id': '598002945', 'photo': urlPhoto}
+# res = requests.get(endPoint, params=params)
+
+# #получить информацию по всем событиям (апдейтам)
+# endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+# param = {'offset': 29751288}
+# response = requests.get(endPoint, params=param).json()
+# pprint.pprint(response)
 
 # usersInfo = dict()
 # for i in response:
