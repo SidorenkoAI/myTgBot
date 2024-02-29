10f1105078ab4b826bf5fb307b6a514efabe7c94
sai <sai@mail.ru>
2024-02-29 20:01:57 +0700

    aiogram first test

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import requests
-import pprint
-with open('token.txt') as f:
+import json
+with open("token.txt") as f:
     token = f.read()
 
 
-def giveAnimation(answer: str) -> str:
-    endPoint = f'https://yesno.wtf/api?force={answer}'
-    res = requests.get(endPoint).json()
-    return res['image']
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=token)
+dp = Dispatcher()
 
 
-import time
-offset = -2
-while True:
-    #получить информацию по всем событиям (апдейтам)
-    endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-    param = {'offset': offset + 1}
-    response = requests.get(endPoint, params=param).json()
-    if response['result']:
-        offset = response['result'][0]['update_id']
-        userText = response['result'][0]['message']['text']
-        chatID = response['result'][0]['message']['chat']['id']
-
-        endPoint = f'https://api.telegram.org/bot{token}/sendAnimation'
-        params = {'chat_id': chatID, "animation": giveAnimation(userText)}
-        res = requests.get(endPoint, params=params)
-        pprint.pprint(response['result'])
-
-    time.sleep(1)
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
 
 
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
 
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
